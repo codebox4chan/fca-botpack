e8bce58f49b19fcabab84dd890c8a4e397ea04af
Reiko Dev <152267140+codebox4chan@users.noreply.github.com>
2024-01-28 08:53:12 +0800

    Update handleCommand.js

--- includes/handle/handleCommand.js
+++ includes/handle/handleCommand.js
@@ -1,47 +1,111 @@
 let activeCmd = false;
 
-module.exports = function({ api, models, Users, Threads, Currencies }) {
+module.exports = function ({ api, models, Users, Threads, Currencies }) {
   const stringSimilarity = require('string-similarity'),
     escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
     logger = require("../../utils/log.js");
   const axios = require('axios')
   const moment = require("moment-timezone");
-  return async function({ event }) {
+  const cooldowns = new Map(); // Create a map to track cooldowns
+  const requestCooldowns = new Map();
+  const cooldownMessagesSent = new Map();
+
+  return async function ({ event }) {
     if (activeCmd) {
       return;
     }
+
     const dateNow = Date.now()
     const time = moment.tz("Asia/Manila").format("HH:MM:ss DD/MM/YYYY");
-    const { allowInbox, PREFIX, ADMINBOT, DeveloperMode, adminOnly, keyAdminOnly } = global.config;
+    const { allowInbox, PREFIX, ADMINBOT, DeveloperMode, adminOnly, keyAdminOnly, OPERATOR, APPROVED, approval } = global.config;
     const { userBanned, threadBanned, threadInfo, threadData, commandBanned } = global.data;
-    const { commands, cooldowns } = global.client;
+    const { commands, cooldowns: clientCooldowns, developermode } = global.client;
+
     var { body, senderID, threadID, messageID } = event;
-    var senderID = String(senderID),
-      threadID = String(threadID);
+    senderID = String(senderID);
+    threadID = String(threadID);
     const threadSetting = threadData.get(threadID) || {}
     const args = (body || '').trim().split(/ +/);
     const commandName = args.shift()?.toLowerCase();
-    var command = commands.get(commandName);
-    const replyAD = '[ MODE ] - Only bot admin can use bot';
+    let command = commands.get(commandName);
 
-    if (command && (command.config.name.toLowerCase() === commandName.toLowerCase()) && (!ADMINBOT.includes(senderID) && adminOnly && senderID !== api.getCurrentUserID())) {
+    const replyAD = "You don't have permission to use bot only admins and bot operators are allowed!";
+    const notApproved = `this box is not approved\n\nuse "${PREFIX}𝗿𝗲𝗾𝘂𝗲𝘀𝘁" to send an approval request to bot operators`;
+
+    if (!APPROVED.includes(threadID) && !OPERATOR.includes(senderID) && !ADMINBOT.includes(senderID) && approval) {
+      setTimeout(() => {
+        api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+      }, 300 * 1000); // 60 seconds = 1 minute
+    }
+
+    if (typeof body === "string" && body.startsWith(`${PREFIX}request`) && approval) {
+      if (APPROVED.includes(threadID)) {
+        return api.sendMessage('This box is already approved', threadID, messageID);
+      }
+
+      // Check if the user has a cooldown for the "request" command
+      const lastRequestTime = requestCooldowns.get(senderID);
+      if (lastRequestTime && dateNow - lastRequestTime < 300000) { // 300000 milliseconds = 5 minutes
+        if (!cooldownMessagesSent.has(senderID)) {
+          api.sendMessage('You can request approval only once every 5 minutes.', threadID, messageID);
+          cooldownMessagesSent.set(senderID, true);
+        }
+        return;
+      } else {
+        // Reset the cooldown message status for the user
+        cooldownMessagesSent.delete(senderID);
+      }
+
+      // Update the last request time for the user
+      requestCooldowns.set(senderID, dateNow);
+
+      let kennethpaniodev;
+      let request;
+      try {
+        const groupname = await global.data.threadInfo.get(threadID).threadName || "𝗧𝗛𝗘 𝗚𝗥𝗢𝗨𝗣 𝗗𝗢𝗘𝗦𝗡'𝗧 𝗛𝗔𝗩𝗘 𝗔 𝗡𝗔𝗠𝗘";
+        kennethpaniodev = `𝗚𝗥𝗢𝗨𝗣 𝗡𝗔𝗠𝗘 : ${groupname}\n𝗧𝗛𝗥𝗘𝗔𝗗 𝗜𝗗 : ${threadID}`;
+        request = `${groupname}\n\nGroup is requesting for approval!.`
+      } catch (error) {
+        const username = await Users.getNameUser(threadID) || "𝗨𝗡𝗞𝗡𝗢𝗪𝗡 𝗨𝗦𝗘𝗥";
+        kennethpaniodev = `𝗨𝗦𝗘𝗥 𝗜𝗗 : ${threadID}`;
+        request = `${username} is requesting for approval`;
+      }
+      return api.sendMessage(`${request}\n\n${kennethpaniodev}`, OPERATOR[0], () => {
+        return api.sendMessage('Your approval request has been sent to bot operators.\n\nStay patient!', threadID, messageID);
+      });
+    }
+
+    if (command && (command.config.name.toLowerCase() === commandName.toLowerCase()) && (!APPROVED.includes(threadID) && !OPERATOR.includes(senderID) && !ADMINBOT.includes(senderID) && approval)) {
+      return api.sendMessage(notApproved, threadID, async (info) => {
+        await new Promise(resolve => setTimeout(resolve, 5 * 1000));
+        return api.unsendMessage(info.messageID);
+      });
+    }
+
+    if (typeof body === 'string' && body.startsWith(PREFIX) && (!APPROVED.includes(threadID) && !OPERATOR.includes(senderID) && !ADMINBOT.includes(senderID) && approval)) {
+      return api.sendMessage(notApproved, threadID, async (info) => {
+        await new Promise(resolve => setTimeout(resolve, 5 * 1000));
+        return api.unsendMessage(info.messageID);
+      });
+    }
+
+    if (command && (command.config.name.toLowerCase() === commandName.toLowerCase()) && (!ADMINBOT.includes(senderID) && !OPERATOR.includes(senderID) && adminOnly && senderID !== api.getCurrentUserID())) {
       return api.sendMessage(replyAD, threadID, messageID);
     }
+
     if (typeof body === 'string' && body.startsWith(PREFIX) && (!ADMINBOT.includes(senderID) && adminOnly && senderID !== api.getCurrentUserID())) {
       return api.sendMessage(replyAD, threadID, messageID);
     }
+
     if (userBanned.has(senderID) || threadBanned.has(threadID) || allowInbox == ![] && senderID == threadID) {
-      if (!ADMINBOT.includes(senderID.toString())) {
+      if (!ADMINBOT.includes(senderID.toString()) && !OPERATOR.includes(senderID.toString())) {
         if (userBanned.has(senderID)) {
           const { reason, dateAdded } = userBanned.get(senderID) || {};
-          return api.sendMessage(global.getText("handleCommand", "userBanned", reason, dateAdded), threadID, async (err, info) => {
-            await new Promise(resolve => setTimeout(resolve, 5 * 1000));
-            return api.unsendMessage(info.messageID);
-          }, messageID);
+          return api.setMessageReaction('⚠️', event.messageID, err => (err) ? logger('An error occurred while executing setMessageReaction', 2) : '', !![]);
         } else {
           if (threadBanned.has(threadID)) {
             const { reason, dateAdded } = threadBanned.get(threadID) || {};
-            return api.sendMessage(global.getText("handleCommand", "threadBanned", reason, dateAdded), threadID, async (err, info) => {
+            return api.sendMessage(global.getText("handleCommand", "threadBanned", reason, dateAdded), threadID, async (info) => {
               await new Promise(resolve => setTimeout(resolve, 5 * 1000));
               return api.unsendMessage(info.messageID);
             }, messageID);
@@ -57,12 +121,12 @@ module.exports = function({ api, models, Users, Threads, Currencies }) {
         if (checker.bestMatch.rating >= 0.5) {
           command = commands.get(checker.bestMatch.target);
         } else {
-          return api.sendMessage(global.getText("handleCommand", "commandNotExist", checker.bestMatch.target), threadID);
+          return api.sendMessage(global.getText("handleCommand", "commandNotExist", checker.bestMatch.target), threadID, messageID);
         }
       }
     }
     if (commandBanned.get(threadID) || commandBanned.get(senderID)) {
-      if (!ADMINBOT.includes(senderID)) {
+      if (!ADMINBOT.includes(senderID) && !OPERATOR.includes(senderID)) {
         const banThreads = commandBanned.get(threadID) || [],
           banUsers = commandBanned.get(senderID) || [];
         if (banThreads.includes(command.config.name))
@@ -94,7 +158,6 @@ module.exports = function({ api, models, Users, Threads, Currencies }) {
       }
     }
 
-
     if (command && command.config && command.config.commandCategory && command.config.commandCategory.toLowerCase() === 'nsfw' && !global.data.threadAllowNSFW.includes(threadID) && !ADMINBOT.includes(senderID))
       return api.sendMessage(global.getText("handleCommand", "threadNotAllowNSFW"), threadID, async (err, info) => {
         await new Promise(resolve => setTimeout(resolve, 5 * 1000))
@@ -108,26 +171,49 @@ module.exports = function({ api, models, Users, Threads, Currencies }) {
       } catch (err) {
         logger(global.getText("handleCommand", "cantGetInfoThread", "error"));
       }
+
     var permssion = 0;
     var threadInfoo = (threadInfo.get(threadID) || await Threads.getInfo(threadID));
     const find = threadInfoo.adminIDs.find(el => el.id == senderID);
-    if (ADMINBOT.includes(senderID.toString())) permssion = 2;
+    if (OPERATOR.includes(senderID.toString())) permssion = 3;
+    else if (ADMINBOT.includes(senderID.toString())) permssion = 2;
     else if (!ADMINBOT.includes(senderID) && find) permssion = 1;
     if (command && command.config && command.config.hasPermssion && command.config.hasPermssion > permssion) {
       return api.sendMessage(global.getText("handleCommand", "permissionNotEnough", command.config.name), event.threadID, event.messageID);
     }
 
-    if (command && command.config && !client.cooldowns.has(command.config.name)) {
-      client.cooldowns.set(command.config.name, new Map());
+    if (command && command.config && !clientCooldowns.has(command.config.name)) {
+      clientCooldowns.set(command.config.name, new Map());
     }
 
-    const timestamps = command && command.config ? client.cooldowns.get(command.config.name) : undefined;
+    const timestamps = command && command.config ? clientCooldowns.get(command.config.name) : undefined;
 
     const expirationTime = (command && command.config && command.config.cooldowns || 1) * 1000;
+    if (timestamps && timestamps.has(senderID)) {
+      if (dateNow < timestamps.get(senderID) + expirationTime) {
+        if (!cooldowns.has(senderID)) {
+          await sendCooldownMessage(api, event, commandName, expirationTime);
+          cooldowns.set(senderID, true);
+        }
+        return;
+      } else {
+        // Reset cooldown state after the cooldown period is over
+        timestamps.delete(senderID);
+      }
+    }
 
-    if (timestamps && timestamps instanceof Map && timestamps.has(senderID) && dateNow < timestamps.get(senderID) + expirationTime)
+    async function sendCooldownMessage(api, event, commandName, cooldownTime) {
+      const cooldownMessage = `𝗔𝗡𝗧𝗜 𝗦𝗣𝗔𝗠 𝗖𝗗⚠️:\nPlease wait ${cooldownTime / 1000} second(s) 🕚 before using it again.`;
+      const cooldownMsg = await api.sendMessage(cooldownMessage, event.threadID);
 
-      return api.setMessageReaction('⏳', event.messageID, err => (err) ? logger('An error occurred while executing setMessageReaction', 2) : '', !![]);
+      // Set a timeout to automatically send a message when cooldown is done
+      setTimeout(async () => {
+        await api.sendMessage(`Cooldown for "${commandName}" is over. You can use it again.`, event.threadID);
+        cooldowns.delete(senderID); // Remove the user from the cooldown list
+        await api.unsendMessage(cooldownMsg.messageID); // Remove the cooldown message
+      }, cooldownTime);
+    }
+
     var getText2;
     if (command && command.languages && typeof command.languages === 'object' && command.languages.hasOwnProperty(global.config.language))
 
@@ -141,7 +227,7 @@ module.exports = function({ api, models, Users, Threads, Currencies }) {
       };
     else getText2 = () => { };
     try {
-      const Obj = { 
+      const Obj = {
         api: api,
         event: event,
         args: args,
@@ -158,12 +244,13 @@ module.exports = function({ api, models, Users, Threads, Currencies }) {
         timestamps.set(senderID, dateNow);
 
         if (DeveloperMode == !![]) {
-                logger(global.getText("handleCommand", "executeCommand", time, commandName, senderID, threadID, args.join(" "), (Date.now()) - dateNow), "DEV MODE");
-              }
-              return;
-            }
-          } catch (e) {
-            return api.sendMessage(global.getText("handleCommand", "commandError", commandName, e), threadID);
+          logger(global.getText("handleCommand", "executeCommand", time, commandName, senderID, threadID, args.join(" "), (Date.now()) - dateNow) + '\n', "command");
+        }
+
+        return;
+      }
+    } catch (e) {
+      return api.sendMessage(global.getText("handleCommand", "commandError", commandName, e), threadID);
     }
     activeCmd = false;
   };
